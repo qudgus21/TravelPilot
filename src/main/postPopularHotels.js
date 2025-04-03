ae982938859863e3f45db2d65d4e2d3bfdbd9fa0
qudgus21 <qudgus21>
2025-04-03 23:24:01 +0900

    feat: @@@@ 메인함수 로직 반영 @@@@

--- src/main/postPopularHotels.js
+++ src/main/postPopularHotels.js
@@ -1,23 +1,39 @@
-//인기숙소 top
-import { crawlHotelByName } from "../crawler/crawlHotelPage.js";
+import fs from "fs";
 import dotenv from "dotenv";
+import minimist from "minimist";
+import cityMap from "../../data/cities.json" assert { type: "json" };
+import { crawlHotelByName } from "../crawler/crawlHotelPage.js";
 import { fetchHotels } from "../agoda/fetchHotels.js";
 import {
   generateCityIntro,
   generateHotelDescription,
 } from "../gpt/generateText.js";
-import cityMap from "../../data/cities.json" assert { type: "json" };
+import {
+  createAffiliateImageTag,
+  createAffiliateLink,
+} from "../agoda/createAffiliateLink.js";
+import { compileTemplate } from "../templates/compileHtml.js";
+import { getCityImage } from "../unsplash/getCityImage.js";
+import { postToWordpress } from "../wordpress/postToWordPress.js";
+
 dotenv.config();
 
-const getCityId = (cityName) => {
-  const cityId = cityMap[cityName];
-  if (!cityId) {
-    throw new Error("❌도시 ID를 찾을 수 없습니다.");
+const getCityId = (inputCityName) => {
+  // 1. 정확히 일치하는 키 우선
+  if (cityMap[inputCityName]) {
+    return cityMap[inputCityName];
   }
 
-  console.log("cityId: ", cityId);
+  // 2. '/로 나눠서 부분 일치 찾기'
+  for (const key of Object.keys(cityMap)) {
+    const parts = key.split("/").map((s) => s.trim());
+    if (parts.includes(inputCityName)) {
+      return cityMap[key];
+    }
+  }
 
-  return cityId;
+  // 3. 실패 시 에러
+  throw new Error(`❌도시 ID를 찾을 수 없습니다: ${inputCityName}`);
 };
 
 const getHotelList = async (cityId) => {
@@ -48,14 +64,14 @@ const crawlHotelDetails = async (cityName, hotelList) => {
       ...detail,
     });
 
-    console.log(`크롤링 성공: ${hotelId}, ${hotelName}`);
+    console.log(`크롤링 성공: ${hotelId}, ${hotelName} ${detail.tiles}`);
   }
 
   console.log("크롤링 완료:", mergedResults.map((h) => h.hotelName).join(", "));
   return mergedResults;
 };
 
-const enrichWithGPT = async (cityName, title, mergedResults) => {
+const getTextByGPT = async (cityName, title, mergedResults) => {
   const cityIntro = await generateCityIntro(cityName, title);
   console.log("\n GPT 도시 설명 \n", cityIntro);
 
@@ -70,27 +86,69 @@ const enrichWithGPT = async (cityName, title, mergedResults) => {
   return { cityIntro, ResultHotelsData: mergedResults };
 };
 
-const run = async () => {
-  const cityName = "뉴욕"; // TODO: 실행 시 입력값으로 대체
-
-  // 1. 도시 이름 → cityId 찾기
-  const cityId = getCityId(cityName);
-
-  // 2. 호텔 리스트 조회
-  const hotelList = await getHotelList(cityId);
-
-  //3. 크롤링
-  const mergedResults = await crawlHotelDetails(cityName, hotelList);
-
-  //4. GPT로 인트로, 숙소설명 얻기
-  const title = `${cityName} 가성비 숙소 TOP ${mergedResults.length}`;
-  const { cityIntro, ResultHotelsData } = await enrichWithGPT(
-    cityName,
-    title,
-    mergedResults
-  );
-
-  // const html = compileTemplate({ title, cityIntro, hotels });
+const addAffiliateLink = (hotels) => {
+  return hotels.map((hotel) => ({
+    ...hotel,
+    affiliateTextLink: createAffiliateLink(hotel.hotelId),
+    affiliateImageTag: createAffiliateImageTag(hotel.hotelId, hotel.imageURL),
+  }));
 };
 
+const run = async () => {
+  //0. 커맨드 입력
+  const args = minimist(process.argv.slice(2), {
+    alias: {
+      c: "city",
+      t: "type",
+    },
+    default: {
+      type: "가성비",
+    },
+  });
+
+  const cityName = args.city;
+  const type = args.type;
+
+  if (!cityName) {
+    console.error("❌ 도시 이름을 입력해주세요. 예: -c 서울");
+    process.exit(1);
+  }
+
+  //1. 도시 이름 → cityId 찾기
+  const cityId = getCityId(cityName);
+
+  //2. 호텔 리스트 조회
+  let hotels = await getHotelList(cityId);
+
+  //3. 크롤링
+  hotels = await crawlHotelDetails(cityName, hotels);
+
+  //4. 어필리에이트 링크 추가
+  hotels = addAffiliateLink(hotels);
+
+  //5. GPT로 도시설명, 인트로 숙소설명 얻기
+  const title = `${cityName} 가성비 숙소 TOP ${hotels.length}`;
+  const { cityIntro, ResultHotelsData } = await getTextByGPT(
+    cityName,
+    title,
+    hotels
+  );
+
+  //6. unsplahs로 도시 사진 얻기
+  const cityImageUrl = await getCityImage(cityName);
+
+  //6. html 생성
+  const html = compileTemplate({
+    title,
+    cityIntro,
+    ResultHotelsData,
+    cityImageUrl,
+  });
+
+  const fileName = `${title}_post.html`;
+  fs.writeFileSync(`./output/${fileName}`, html, "utf-8");
+
+  //7. 블로그 포스팅
+  await postToWordpress(html, title, cityImageUrl);
+};
 run();
